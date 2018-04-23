8257c65ac8e3b5f0e8669c0bfcdd597e0028b97c
Farzad Rastegar <farzadras@gmail.com>
2018-04-23 19:29:15 +0430

    Add ftp to post-pxe

--- scripts/post-pxe-config.bash
+++ scripts/post-pxe-config.bash
@@ -2,6 +2,11 @@
 systemctl enable firewalld
 systemctl start firewalld
 
+firewall-cmd --add-port=21/tcp --permanent
+firewall-cmd --add-service=ftp --permanent
+firewall-cmd --reload
+systemctl restart vsftpd
+
 firewall-cmd --add-service=dhcp --permanent
 firewall-cmd --reload
 systemctl restart dhcpd
