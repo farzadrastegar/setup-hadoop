a6dcce5ade27430b92d55db6f9ca92f71c8d53be
Farzad Rastegar <farzadras@gmail.com>
2018-05-05 22:50:23 +0430

    Edit script for installing java

--- scripts/samples/install-java-jdk-8u151.bash
+++ scripts/samples/install-java-jdk-8u151.bash
@@ -8,11 +8,11 @@ java -version
 
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.rpm"
 
-yum localinstall jdk-8u151-linux-x64.rpm
+yum localinstall jdk-8u151-linux-x64.rpm -y
 alternatives --config java <<< ${javaOption}
 
-sh -c "echo export JAVA_HOME=/usr/java/jdk1.8.0_151/jre >> ~/.bashrc"
-sh -c "echo export JAVA_HOME=/usr/java/jdk1.8.0_151/jre >> /etc/environment"
+sh -c "echo export JAVA_HOME=/usr/java/default >> ~/.bashrc"
+sh -c "echo export JAVA_HOME=/usr/java/default >> /etc/environment"
 
 java -version
 
